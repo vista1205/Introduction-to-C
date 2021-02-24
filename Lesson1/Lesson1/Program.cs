c706abdd1616a3e65f782a4aaa2f25087c7eeeb6
vista1205 <vista35@vtomske.ru>
2021-02-24 11:56:24 +0700

    Done task 1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Написать программу, выводящую в консоль текст: «Привет, %имя пользователя%, сегодня %дата%». Имя пользователя сохранить из консоли в промежуточную переменную. Поставить точку останова и посмотреть значение этой переменной в режиме отладки. Запустить исполняемый файл через системный терминал.
+            Console.Write("Введите ваше имя: ");
+            string UserName = Console.ReadLine();
+            Console.WriteLine($"Здравствуй, {UserName}! Сегодня {DateTime.Now}");
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
