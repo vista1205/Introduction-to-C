911ab15c04afa8bc35b096fa518ec101cae47617
vista1205 <vista35@vtomske.ru>
2021-02-24 12:49:09 +0700

    Done task 1

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+            Console.WriteLine("Необходимо ввести максимальную и минимальную температуру, для расчёта средней температуры");
+            Console.Write("Введите максимальную температуру: ");
+            float MaxTemp = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите минимальную темепературу: ");
+            float MinTemp = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine($"Средняя температура равна: {(MaxTemp + MinTemp)/2}");
+            #endregion
+        }
+    }
+}
