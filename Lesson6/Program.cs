c3a3fbbc6a584e1978586cd800e0ecf034c583b8
vista1205 <vista35@vtomske.ru>
2021-03-14 19:32:57 +0700

    Create 6 task

---
+++ Lesson6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Process process in Process.GetProcesses())
+                Console.WriteLine($"ID: {process.Id}\tName: {process.ProcessName}");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Process.GetProcessById(a).Kill();
+            Console.ReadKey();
+        }
+    }
+}
