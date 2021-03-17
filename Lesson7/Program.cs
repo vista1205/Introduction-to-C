9b2a23988feb892e39b0a9ca7c6840ce439be0b7
vista1205 <vista35@vtomske.ru>
2021-03-17 21:12:22 +0700

    Done all task 7 lesson

---
+++ Lesson7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+    class Program
+    {
+        public static int a = 120;
+        public static int b = 140;
+        static void Main(string[] args)
+        {
+            for(int i = a; i <= b; i++)
+            {
+                int num = 0;
+                for(int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                        num++;
+                }
+                Console.WriteLine($"У числа {i} {num} делителей");
+            }
+            Console.ReadKey();
+        }
+    }
+}
