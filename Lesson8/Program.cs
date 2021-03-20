12440a17d14d8ad145fa734a7191ddc2696a0c38
Vitalii Kozlov <vista35@vtomske.ru>
2021-03-20 12:26:51 +0700

    80%

---
+++ Lesson8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName))
+            {
+                Console.Write("Введите Ваше имя: ");
+                Properties.Settings.Default.UserName = Console.ReadLine();
+                Console.Write("Введите Ваш возраст: ");
+                Properties.Settings.Default.UserAge = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Чeм вы занимаетесь: ");
+                Properties.Settings.Default.userAbout = Console.ReadLine();
+                Properties.Settings.Default.Save();
+
+            }
+            string privet = Properties.Settings.Default.Greeting;
+            string userName = Properties.Settings.Default.UserName;
+            int userAge = Properties.Settings.Default.UserAge;
+            string UserAbout = Properties.Settings.Default.userAbout;
+            Console.WriteLine($"{privet}\n{userName}\n{userAge}\n{UserAbout}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Урок 8. Настройка приложения")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GekkBrains")]
+[assembly: AssemblyProduct("Lesson8")]
+[assembly: AssemblyCopyright("Copyright ©vista  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("99df7d38-f616-4556-a046-22dea290b583")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
